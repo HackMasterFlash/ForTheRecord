ea9421234833976f38e936f94cf501f132aaf2be
Stephen Trickey <itstrickey@gmail.com>
2025-04-08 20:41:58 -0400

    using dotenv to load secret keys

--- config.py
+++ config.py
@@ -1,27 +1,23 @@
 # config.py
 ### This file is used to do flask configuration
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the environment variables using os.environ
+# api_key = os.environ.get("API_KEY")
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-# import connexion
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_marshmallow import Marshmallow
-# connex_app = connexion.App(__name__, specification_dir=basedir)
-
-# app = connex_app.app
-# app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'media.db'}"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# db = SQLAlchemy(app)
-# ma = Marshmallow(app)
-
 # This version comes from the book "Mastering Flask Web Development" by Daniel Gaspar and Jack Stouffer
 class Config(object): 
-    SECRET_KEY = 'my_secret_key_to_change'
-    RECAPTCHA_PUBLIC_KEY = "6LdKkQQTAAAAAE_also_to_change"
-    RECAPTCHA_PRIVATE_KEY = '6LdKkQQTAAAAAMYroks_also_to_change'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    OMDB_API_KEY = os.environ.get("OMDB_API_KEY")
+    RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY")
+    RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
     POSTS_PER_PAGE = 10 
  
 class ProdConfig(Config): 
@@ -29,6 +25,8 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db') 
  
 class DevConfig(Config): 
-    DEBUG = True
+    # DEBUG = True
+    DEBUG = os.environ.get("DEBUG")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
