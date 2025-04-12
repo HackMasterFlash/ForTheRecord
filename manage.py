703aec2d291efc73070dd4d7a2f20281f91a6307
Stephen Trickey <itstrickey@gmail.com>
2025-04-12 10:04:45 -0400

    coverting to values from my project

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 import os
 from webapp import db, migrate, create_app
-from webapp.auth.models import User
-from webapp.media.models import Post, Tag
+from webapp.media.models import Actor, Movie, Director
 
 
 env = os.environ.get('WEBAPP_ENV', 'dev')
@@ -10,4 +9,4 @@ app = create_app('config.%sConfig' % env.capitalize())
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post, Tag=Tag, migrate=migrate)
+    return dict(app=app, db=db, Actor=Actor, Movie=Movie, Director=Director, migrate=migrate)
