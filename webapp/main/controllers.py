09e732f8b907f98e5bd8f6df178e6fffe18afb7f
Stephen Trickey <itstrickey@gmail.com>
2025-04-19 11:27:00 -0400

    spin up the main home page the way I was thinking of with a movie title search box

--- webapp/main/controllers.py
+++ webapp/main/controllers.py
@@ -1,12 +1,41 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, request
 
 main_blueprint = Blueprint(
     'main',
     __name__,
-    template_folder='../templates/main'
+    template_folder='../templates'
 )
 
 
-@main_blueprint.route('/')
-def index():
-    return redirect(url_for('media.home'))
+@main_blueprint.route('/', methods=['GET', 'POST'])
+def home():
+    """The home page of the web app."""
+    if request.method == 'POST':
+        # Handle POST request
+        # Access form data
+        form_data = request.form
+        media_title = form_data.get('Title')
+        search_type = form_data.get('radioSearchSelect')
+        if search_type.strip() == 'OMDB':
+            # Call OMDB API with the selected search type
+            # Example: Call the API with the title and search type
+            # response = call_omdb_api(media_title, search_type)
+            # Process the response and render the results
+            # return render_template('results.html', results=response)
+            # For now, just return a placeholder response
+            return render_template('results.html', results="Placeholder response")
+        else:
+            # Look up this title in the local database using SQLAlchemy
+            # Example: Call the database query with the title
+            # response = query_local_database(media_title)
+            # Process the response and render the results
+            # return render_template('results.html', results=response)
+            # For now, just return a placeholder response
+            return render_template('results.html', results="Placeholder response")
+        # If no search type is selected, return an error message or redirect
+        return redirect(url_for('.home'))
+        # return "Error: No search type selected"
+    
+    else:
+        # Handle GET request
+        return render_template('home.html')    
