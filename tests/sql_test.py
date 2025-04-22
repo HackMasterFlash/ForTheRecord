522e8577280203fcb1cca423676a3e2ee4d45833
Stephen Trickey <itstrickey@gmail.com>
2025-04-21 23:03:00 -0400

    test out the new model implementations. AFter some fineagling, think I got this to work.

--- tests/sql_test.py
+++ tests/sql_test.py
@@ -9,7 +9,7 @@ module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if module_path not in sys.path:
     sys.path.insert(0, module_path)  # Add to the beginning for priority
 
-from webapp.media.models import db, Actor, Movie, Director
+from webapp.media.models import db, Actor, Movie, Director, Writer
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,7 +17,7 @@ load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Define the database URL (replace with your actual database URL)
-DATABASE_URL = 'sqlite:///' + os.path.join('..', 'instance', 'mydatabase.db')
+DATABASE_URL = 'sqlite:///' + os.path.join(basedir,'..', 'instance', 'mydatabase.db')
 print('Trying to connect to database {0}'.format(DATABASE_URL))
 
 # Create a SQLAlchemy engine
@@ -36,7 +36,8 @@ inspector = inspect(engine)
 noActors = not inspector.has_table('actors')
 noMovies = not inspector.has_table('movies')
 noDirectors = not inspector.has_table('directors')
-if ( noActors or noMovies or noDirectors):
+noWriters = not inspector.has_table('writers')
+if ( noActors or noMovies or noDirectors or noWriters):
     db.metadata.create_all(engine)
     print('Creating table definitions')
 else:
@@ -53,14 +54,27 @@ db_local = session()
 try:
     # Create some actors
     actor1 = Actor(first_name="Leonardo", last_name="DiCaprio")
-    actor2 = Actor(first_name="Brad", last_name="Pitt")
-    actor3 = Actor(first_name="Kevin", last_name="Bacon")
+    actor2 = Actor.ActorFactory("Brad Pitt")
+    actor3 = Actor.ActorFactory("Kevin Bacon")
     actor4 = Actor(first_name="Tom", last_name="Hanks")
+    actor5 = Actor(first_name="Roy", last_name="Scheider")
+    actor6 = Actor(first_name="Robert", last_name="Shaw")
+    actor7 = Actor(first_name="Richard", last_name="Dreyfuss")
+    actor8 = Actor(first_name="Bill", last_name="Paxton")
 
     # Create some directors
     director1 = Director(first_name="Christopher", last_name="Nolan")
     director2 = Director(first_name="Ron", last_name="Howard")
     director3 = Director(first_name="Robert", last_name="Zemeckis")
+    director4 = Director.DirectorFactory("Steven Spielberg")
+
+    # Create some writers
+    writer1 = Writer(first_name="Peter", last_name="Benchley")
+    writer2 = Writer(first_name="Carl", last_name="Gottlieb")
+    writer3 = Writer(first_name="Christopher", last_name="Nolan")
+    writer4 = Writer(first_name="Jim", last_name="Lovell")
+    writer5 = Writer(first_name="Jeffrey", last_name="Luger")
+    writer6 = Writer.WriterFactory("William Broyles Jr.")
 
     actors_to_add = [actor1, actor2, actor3, actor4]
     something_to_commit = False
@@ -78,15 +92,33 @@ try:
         db_local.commit()
         something_to_commit = False
 
+    writers_to_add = [writer1, writer2, writer3, writer4, writer5, writer6]
+    something_to_commit = False
+    for a_writer in writers_to_add:
+        inTable = db_local.query(Writer).filter(
+            Writer.first_name == a_writer.first_name,
+            Writer.last_name == a_writer.last_name).first()
+        if inTable:
+            print('Writer {0} {1} already in table'.format(a_writer.first_name, a_writer.last_name))
+        else:
+            db_local.add(a_writer)
+            something_to_commit = True
+
+    if something_to_commit:
+        db_local.commit()
+        something_to_commit = False
+
     # Create some movies and associate actors
-    movie1 = Movie(title="Inception", director=director1, actors=[actor1, actor2])
+    movie1 = Movie(title="Inception", director=director1, actors=[actor1, actor2], writers=[writer3])
     movie2 = Movie(title="Apollo 13", director=director2, 
-                   actors=[actor3, actor4],
+                   actors=[actor3, actor4, actor8],
+                   writers=[writer4, writer5, writer6],
                    year=1995, IsMovie=True,
                    PersonalRating=10)
-    movie3 = Movie(title="Forrest Gump", director=director3, actors=[actor4])
+    # movie3 = Movie(title="Forrest Gump", director=director3, actors=[actor4])
+    movie4 = Movie(title="Jaws", director=director4, actors=[actor7,actor5,actor6], writers=[writer1,writer2])
     
-    new_movies = [movie1, movie2, movie3]
+    new_movies = [movie1, movie2, movie4]
     for a_movie in new_movies:
         inTable = db_local.query(Movie).filter(
             Movie.title == a_movie.title).first()
@@ -112,6 +144,13 @@ try:
     for actor in actors:
         movie_titles = [movie.title for movie in actor.movies]
         print(f"Actor: {actor.first_name} {actor.last_name}, Movies: {', '.join(movie_titles)}")
+
+    # Query writers and the movies they wrote for
+    print("\nWriters and their Movies:")
+    writers = db_local.query(Writer).all()
+    for writer in writers:
+        movie_titles = [movie.title for movie in writer.movies]
+        print(f"Writer: {writer.first_name} {writer.last_name}, Movies: {', '.join(movie_titles)}")
 
 except Exception as e:
     print(f"An error occurred: {e}")
